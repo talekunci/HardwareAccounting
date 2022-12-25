27fbc1769fa32542c4ab1c20fbaaf9f0d96da94b
talekunci <smouke.king1@gmail.com>
2022-12-25 14:39:58 +0200

    Git init

---
+++ src/main/java/com/accounting/HardwareAccounting/HardwareAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.HardwareAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accounting/HardwareAccounting/HardwareAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.HardwareAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
