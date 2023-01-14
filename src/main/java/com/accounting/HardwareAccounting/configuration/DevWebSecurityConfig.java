9d40799f4f35bab86a009a658ad7d924d52038b2
talekunci <smouke.king1@gmail.com>
2023-01-14 02:29:30 +0200

    Created pre-release security configs

---
+++ src/main/java/com/accounting/HardwareAccounting/configuration/DevWebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.accounting.HardwareAccounting.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+public class DevWebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2/**").permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+}
